d083e0995efa287d870dce297eea045a3100fe01
rishsol <rishabsolanki09@gmail.com>
2022-01-11 19:54:10 -0500

    first commit

---
+++ contracts/Poll.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.21 <8.10.0;
+
+contract Poll {
+
+    struct Coin {
+        uint id;
+        string name;
+        uint firstPlaceCount;
+        uint secondPlaceCount;
+        uint thirdPlaceCount;
+    }
+
+    mapping(address => bool) public voters;
+
+    mapping(uint => Coin) public coins;
+
+    uint public coinCount;
+
+    event rankedEvent (
+        uint[] indexed _coinIds
+    );
+
+    constructor() public{
+        addCoin("BTC");
+        addCoin("ETH");
+        addCoin("AVAX");
+    }
+
+    function addCoin(string memory _name) private {
+        coinCount++;
+        coins[coinCount] = Coin(coinCount, _name, 0, 0, 0);
+    }
+
+    function getTotalCoins() public view returns (uint) {
+        return coinCount;
+    }
+
+    function rank(uint[] memory coinIds) public {
+        require(!voters[msg.sender]);
+        require(coinIds.length >= 3);
+
+        voters[msg.sender] = true;
+
+        for (uint i = 0; i < 3; i++) {
+            require(i < coinIds.length && coinIds[i] > 0 && coinIds[i] <= coinCount);
+        }
+
+        coins[coinIds[0]].firstPlaceCount += 1;
+        coins[coinIds[1]].secondPlaceCount += 1;
+        coins[coinIds[2]].thirdPlaceCount += 1;
+
+        emit rankedEvent(coinIds);
+    }
+
+}
---
+++ test/poll.js
@@ -0,0 +1,34 @@
+const Poll = artifacts.require("./Poll.sol");
+
+contract("Poll", (accounts) => {
+  let poll = null;
+
+  before(async () => {
+    poll = await Poll.deployed();
+  });
+
+  it("initiates polling with 3 coins", async () => {
+    const count = await poll.getTotalCoins.call();
+    assert.equal(count, 3, "3 coins not created");
+  });
+
+  it("allows users to rank coins appropriately", async () => {
+    await poll
+      .rank([3, 2, 1], { from: accounts[0] })
+      .then(function (instance) {
+        return poll.coins(1);
+      })
+      .then(function (coin) {
+        assert.equal(coin[4], 1, "third place count should be 1");
+        return poll.coins(2);
+      })
+      .then(function (coin) {
+        assert.equal(coin[3], 1, "second place count should be 1");
+        return poll.coins(3);
+      })
+      .then(function (coin) {
+        assert.equal(coin[1], 'AVAX', 'This should be avalanche coin');
+        assert.equal(coin[2], 1, "first place count should be 1");
+      });
+  });
+});
