2550de2d6181fb84342deab6fe1b740c61f1e0c2
rishsol <rishabsolanki09@gmail.com>
2022-02-25 09:31:58 -0500

    fix tests and add style changes

--- test/TestVoterLinkedList.sol
+++ test/TestVoterLinkedList.sol
@@ -6,14 +6,24 @@ import "../contracts/VoterLinkedList.sol";
 
 contract TestVoterLinkedList {
     function testAdd() public {
-    VoterLinkedList vll = VoterLinkedList(DeployedAddresses.VoterLinkedList());
+        VoterLinkedList vll = VoterLinkedList(
+            DeployedAddresses.VoterLinkedList()
+        );
 
-    vll.add((address) (0x111));
-    Assert.equal(vll.getSize(), 1, "Size should be 1.");
+        vll.add((address)(0x111));
+        Assert.equal(vll.getSize(), 1, "Size should be 1.");
 
-    vll.add((address) (0x112));
-    Assert.equal(vll.getSize(), 2, "Size should be 2.");
-    Assert.equal(vll.getHead().getName(), (address) (0x112), "Name should be 0x112");
-    Assert.equal(vll.getHead().getNext().getName(), (address) (0x111), "second item name should be 0x111");
-  }
+        vll.add((address)(0x112));
+        Assert.equal(vll.getSize(), 2, "Size should be 2.");
+        Assert.equal(
+            vll.getHead().getName(),
+            (address)(0x112),
+            "Name should be 0x112"
+        );
+        Assert.equal(
+            vll.getHead().getNext().getName(),
+            (address)(0x111),
+            "second item name should be 0x111"
+        );
+    }
 }
